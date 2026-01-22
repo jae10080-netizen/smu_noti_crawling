e7b84961b4aa4e558412e73dd426af31813b4ac7
jae10080-netizen <jae10080@gmail.com>
2026-01-22 21:46:41 +0900

    Update 크롤링.py

--- 크롤링.py
+++ 크롤링.py
@@ -12,7 +12,16 @@ import pandas as pd
 import bs4
 
 import requests
-
+from feedgen.feed import FeedGenerator
+fg = FeedGenerator()
+fg.id('http://lernfunk.de/media/654321')
+fg.title('Some Testfeed')
+fg.author( {'name':'John Doe','email':'john@example.de'} )
+fg.link( href='http://example.com', rel='alternate' )
+fg.logo('http://ex.com/logo.jpg')
+fg.subtitle('This is a cool feed!')
+fg.link( href='http://larskiesow.de/test.atom', rel='self' )
+fg.language('en')
 
 
 
